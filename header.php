8c5d9280644bf6998b8bec3e63ae7963b7becf4b
I m r a a n <imrn.dev@gmail.com>
2026-09-11 00:52:59 +0530

    Site-wide palette swap: slate navy / azure / violet aurora

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="theme-color" content="#0e0d0d">
+	<meta name="theme-color" content="#0f172a">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
