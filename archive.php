2778fff5f1bb1b6e4d06f5bc5f3775fbf7ee92b6
I m r a a n <imrn.dev@gmail.com>
2026-09-13 18:07:01 +0530

    Author archive: add author identity hero card with bio, stats and social links

--- archive.php
+++ archive.php
@@ -20,17 +20,38 @@ $archive = v5imraan_archive_heading();
 		<div class="container">
 			<?php v5imraan_the_breadcrumbs(); ?>
 			<p class="blog-hero__kicker"><?php echo esc_html( $archive[0] ); ?></p>
-			<h1 class="blog-hero__title"><?php echo esc_html( $archive[1] ); ?></h1>
-			<?php
-			$archive_desc = get_the_archive_description();
-			if ( $archive_desc ) :
-				?>
-				<div class="blog-hero__tagline"><?php echo wp_kses_post( $archive_desc ); ?></div>
-			<?php endif; ?>
+
 			<?php if ( is_author() ) : ?>
-				<div class="blog-hero__author">
-					<?php echo get_avatar( get_queried_object_id(), 56, '', $archive[1] ); ?>
+				<?php $author_count = (int) $GLOBALS['wp_query']->found_posts; ?>
+				<div class="author-hero">
+					<div class="author-hero__avatar">
+						<?php echo get_avatar( get_queried_object_id(), 128, get_stylesheet_directory_uri() . '/img/imrn.png', $archive[1] ); ?>
+					</div>
+					<div class="author-hero__body">
+						<h1 class="blog-hero__title author-hero__title"><?php echo esc_html( $archive[1] ); ?></h1>
+						<p class="author-hero__role"><?php esc_html_e( 'Engineering Leader & AI Architect', 'v5imraan' ); ?></p>
+						<p class="author-hero__bio"><?php echo esc_html( v5imraan_get_author_bio( get_queried_object_id() ) ); ?></p>
+						<ul class="author-hero__meta">
+							<li class="author-hero__stat">
+								<span class="author-hero__stat-number"><?php echo esc_html( number_format_i18n( $author_count ) ); ?></span>
+								<?php echo esc_html( _n( 'Article', 'Articles', $author_count, 'v5imraan' ) ); ?>
+							</li>
+							<?php foreach ( v5imraan_author_social_links() as $network_label => $network_url ) : ?>
+								<li>
+									<a class="author-hero__chip" href="<?php echo esc_url( $network_url ); ?>" target="_blank" rel="noopener noreferrer"><?php echo esc_html( $network_label ); ?></a>
+								</li>
+							<?php endforeach; ?>
+						</ul>
+					</div>
 				</div>
+			<?php else : ?>
+				<h1 class="blog-hero__title"><?php echo esc_html( $archive[1] ); ?></h1>
+				<?php
+				$archive_desc = get_the_archive_description();
+				if ( $archive_desc ) :
+					?>
+					<div class="blog-hero__tagline"><?php echo wp_kses_post( $archive_desc ); ?></div>
+				<?php endif; ?>
 			<?php endif; ?>
 		</div>
 	</section>
